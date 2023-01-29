2240f5ccd06fb483214895e4488837ef3b96fb73
deddyrahmat <deddyrahmat97@gmail.com>
2023-01-29 11:30:37 +0700

    add navigate
    add routing and setup navigation app

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import {GetStarted, Splash} from './../pages';
+
+const Stack = createStackNavigator();
+function Router() {
+  return (
+    <Stack.Navigator initialRouteName='Splash'>
+        <Stack.Screen name='Splash' component={Splash} options={{headerShown : false}} />
+        <Stack.Screen name='GetStarted' component={GetStarted} options={{headerShown : false}} />
+    </Stack.Navigator>
+  )
+}
+
+export default Router
